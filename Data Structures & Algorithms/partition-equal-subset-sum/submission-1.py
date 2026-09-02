b81a7129bfcfa28dafcc979f4b2ee18f22a44ce1
Aryan katiyar <aryankatiyar642@gmail.com>
2026-09-02 16:36:22 +0530

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.py
@@ -0,0 +1,18 @@
+from functools import cache
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        summ = sum(nums)
+        if summ %2 != 0:
+            return False
+        
+        target = summ // 2
+        @cache
+        def solve(index, target):
+            if index == len(nums) or target < 0:
+                return False
+            if target == 0:
+                return True
+            
+            return solve(index+1,target-nums[index]) or solve(index+1,target)
+
+        return solve(0,target)
