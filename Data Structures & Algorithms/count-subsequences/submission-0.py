8270ade47fdd3270d170fc8c767c7ec72b439f91
Aryan katiyar <aryankatiyar642@gmail.com>
2026-09-05 22:41:28 +0530

    Add: count-subsequences - submission-0

---
+++ Data Structures & Algorithms/count-subsequences/submission-0.py
@@ -0,0 +1,17 @@
+from functools import cache
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        @cache
+        def solve(i,j):
+            if j == len(t):
+                return 1
+            if i >= len(s):
+                return  0
+            ans = 0
+
+            if s[i] == t[j]:
+                ans += solve(i+1,j+1)
+
+            ans += solve(i+1,j)
+            return ans
+        return solve(0,0)
